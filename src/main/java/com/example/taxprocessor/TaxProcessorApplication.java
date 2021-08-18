90592f4449bdabe1f1bd4db1624a641ad7836678
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2021-08-18 20:12:17 +0900

    first commit

---
+++ src/main/java/com/example/taxprocessor/TaxProcessorApplication.java
@@ -0,0 +1,31 @@
+package com.example.taxprocessor;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+@SpringBootApplication
+public class TaxProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxProcessorApplication.class, args);
+	}
+
+	@KafkaListener(id = "myId", topics = "originalPrice")
+	@SendTo("taxAddedPrice")
+	public long listen(long price) {
+		return Math.round(price * 1.1);
+	}
+
+	@Bean
+	public ApplicationRunner runner(KafkaTemplate<String, Long> template) {
+		return args -> {
+			template.send("originalPrice", 200L);
+		};
+	}
+
+}
---
+++ src/test/java/com/example/taxprocessor/TaxProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
