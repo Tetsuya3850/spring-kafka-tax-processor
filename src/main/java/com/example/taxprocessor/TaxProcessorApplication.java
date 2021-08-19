84697b1bdbf168e78ed4f3522ecea65f8cab7f2b
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2021-08-19 09:19:49 +0900

    optimize imports

--- src/main/java/com/example/taxprocessor/TaxProcessorApplication.java
+++ src/main/java/com/example/taxprocessor/TaxProcessorApplication.java
@@ -1,11 +1,8 @@
 package com.example.taxprocessor;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.SendTo;
 
 @SpringBootApplication
